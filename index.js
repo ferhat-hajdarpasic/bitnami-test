0c443921533c4d73b684b1b2c59a4b7674d2086f
Fred Hajdarpasic <fred.hajdarpasic@outlook.com>
2021-01-04 00:47:57 +1100

    Sending to airtracker

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
-const express = require('express');  
-const bodyParser = require('body-parser');  
-const url = require('url');  
+const requestModule = require('request');
+const express = require('express');
+const bodyParser = require('body-parser');
+const url = require('url');
 const querystring = require('querystring');
 
 let winston = require('winston');
@@ -16,40 +17,56 @@ let logger = winston.createLogger({
     ),
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({filename: 'test.log'})
+        new winston.transports.File({ filename: 'test.log' })
     ]
 });
 
 let app = express();
-const API_SERVICE_URL = process.env.API_SERVICE_URL;  
-app.use(bodyParser.urlencoded({ extended: false }));  
+const API_SERVICE_URL = process.env.API_SERVICE_URL;
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Function to handle the root path
-app.get('/', async function(req, res) {
+app.get('/', async function (req, res) {
     logger.log('info', JSON.stringify(req.query));
-    res.writeHead(200, {'Content-type':'text/plain'});
+    res.writeHead(200, { 'Content-type': 'text/plain' });
     res.end('Hello World');
 });
 
 app.use('/michael', createProxyMiddleware({
-   target: API_SERVICE_URL,
-   changeOrigin: true,
-   pathRewrite: {
-       [`^/michael`]: '',
-   }
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: {
+        [`^/michael`]: '',
+    }
 }));
 
-app.post('/demo', function(request, response){
+app.post('/demo', function (request, response) {
     logger.log('info', request.body);
-    response.send({
-        device: request.body.device,
-        time: request.body.time,
-        data: request.body.data,
-        seqNumber: request.body.seqNumber
-    });
-  });
+    const form = {
+        cbkey: process.env.CBKEY,
+        batch: `${request.body.device};${request.body.time};${request.body.data};${request.body.seqNumber}`
+    };
+    const formData = querystring.stringify(form);
+    const contentLength = formData.length;
 
-let server = app.listen(8080, function() {  
+    requestModule.post({
+        headers: {
+            'Content-Length': contentLength,
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'host': process.env.AIRTRACKER
+        },
+        uri: process.env.AIRTRACKER_URL,
+        body: formData
+    }, function (err, res, body) {
+        response.status(res.statusCode).send({
+            err: err,
+            res: res,
+            body: body
+        });
+    });
+});
+
+let server = app.listen(8080, function () {
     logger.log('info', 'Server is listening on port 8080')
 });
